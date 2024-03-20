993ba2b25996a9f3e1f4a084df588f35fe1b29ba
chicken8848 <mech_4e@hotmail.com>
2024-03-20 22:57:50 +0800

    feat: regfile_controller

---
+++ FPGA/regfile_controller/regfile_controller.v
@@ -0,0 +1,12 @@
+module regfile_controller (
+  input [31:0] id,
+  output [4:0] ra1,
+  output [4:0] ra2,
+  output [4:0] wa
+);
+
+assign ra1 = id[19:15];
+assign ra2 = id[24:20];
+assign wa = id[11:7];
+
+endmodule
